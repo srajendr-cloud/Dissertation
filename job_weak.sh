9a4c05df7f5434ac148798fc717977d39303ab49
srajendr-cloud <srajendr@tcd.ie>
2026-08-21 11:30:46 +0100

    Extend scaling experiments to 16 and 32 processes across four nodes

--- job_weak.sh
+++ job_weak.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=heat_weak_scaling
 #SBATCH --output=heat_weak_scaling_%j.out
-#SBATCH --ntasks=8
+#SBATCH --nodes=4
+#SBATCH --ntasks=32
+#SBATCH --ntasks-per-node=8
 #SBATCH --time=00:30:00
 
 module load openmpi
 export LD_LIBRARY_PATH=/home/support/rl8/spack/1.1.1/opt/spack/linux-x86_64_v3/openmpi-5.0.9-2irqibqfnnap6fptp26b7bnwf5qhybuq/lib:$LD_LIBRARY_PATH
 
-np_2d=(1 2 4 8)
-n_2d=(40 57 80 113)
+np_2d=(1 2 4 8 16 32)
+n_2d=(40 57 80 113 160 226)
 
-for i in 0 1 2 3
+for i in 0 1 2 3 4 5
 do
     np=${np_2d[$i]}
     n=${n_2d[$i]}
@@ -19,7 +21,7 @@ do
     echo ""
 done
 
-for i in 0 1 2 3
+for i in 0 1 2 3 4 5
 do
     np=${np_2d[$i]}
     n=${n_2d[$i]}
@@ -28,10 +30,10 @@ do
     echo ""
 done
 
-np_3d=(1 2 4 8)
-n_3d=(20 25 32 40)
+np_3d=(1 2 4 8 16 32)
+n_3d=(20 25 32 40 50 63)
 
-for i in 0 1 2 3
+for i in 0 1 2 3 4 5
 do
     np=${np_3d[$i]}
     n=${n_3d[$i]}
@@ -40,7 +42,7 @@ do
     echo ""
 done
 
-for i in 0 1 2 3
+for i in 0 1 2 3 4 5
 do
     np=${np_3d[$i]}
     n=${n_3d[$i]}
